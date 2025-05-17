181aa0831793ebf88423c4e513f7951f441310af
Matthias Seitz <matthias.seitz@outlook.de>
2025-05-17 06:36:30 +0200

    chore: fix clippy

--- remote/src/codec.rs
+++ remote/src/codec.rs
@@ -144,7 +144,7 @@ impl TryFrom<&reth::primitives::TransactionSigned> for proto::Transaction {
             s: transaction.signature().s().to_le_bytes_vec(),
             y_parity: transaction.signature().v(),
         };
-        let transaction = match &transaction {
+        let transaction = match &transaction.clone().into_typed_transaction() {
             reth::primitives::Transaction::Legacy(alloy_consensus::TxLegacy {
                 chain_id,
                 nonce,
